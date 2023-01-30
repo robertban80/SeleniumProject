11298577f31743175f210a835e87bbd3db9d20d8
robert.ban <robertban80@gmsil.com>
2023-01-30 20:40:29 +0200

    Lab 31 - Automation 11 - 30.01.2023

---
+++ src/test/java/com/google/GoogleTest.java
@@ -0,0 +1,36 @@
+package com.google;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+    @Test
+    public void testGoogle(){
+        //creare driver
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //deschidem pagina care trebuie testata
+        String url = "https://google.com/";
+        driver.get(url);
+        System.out.println("Opening google.com page...");
+
+        //maximizare fereastra
+        driver.manage().window().maximize();
+        System.out.println("Maximizeaza fereastra...");
+
+        // verific pe ce pagina sunt
+        //Assert.assertEquals(driver.getCurrentUrl(),url); // returneaza url-ul curent din chrome
+        Assert.assertEquals("https://google.com/",url);
+
+        //inchide fereastra cu browser-ul
+        driver.quit();
+        System.out.println("Inchid fereastra cu google.com");
+
+
+
+
+    }
+}
